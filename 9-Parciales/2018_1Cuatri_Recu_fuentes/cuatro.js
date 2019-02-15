100609c51d39e1a3534db93a6ed422efc4082c1d
alumno <alumno>
2019-02-15 11:13:46 -0300

    teminer los 4 ejercios de la prueba pero falta corregir

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+	var cantidadDeLibros;
+	var precioDeLoslibros
+	var cuenta;
+	var tarjetaOefectivo;
+
+
+	cantidadDeLibros =	prompt("Cuatos libros compro?");
+	
+	precioDeLoslibros =  prompt("ingrese el precio total de los libro");
+
+	tarjetaOefectivo = prompt("Si paga con efectivo ponga 1 si para con tarjeta ponga 2");
+
+	if (cantidadDeLibros > 5) {
+		precioDeLoslibros = precioDeLoslibros * 0.10; 
+
+	} if (precioDeLoslibros > 2000) {
+		precioDeLoslibros = precioDeLoslibros * 0.15;
+	}
+
+	if (tarjetaOefectivo == 2 ) {
+		precioDeLoslibros = precioDeLoslibros + 0.10;
+	}
+
+	alert(precioDeLoslibros)
+
+
 
 }
+
+/*
+ en una tienda de libros existe una oferta por el cual si compra mas de los libros tiene un 
+ 10% en precio y si ademas supera los 2000$ se le hace un 15% 
+ de descuento adicional recordar que si la compra es con tarjeta se debera agregar un 10% de recargo
+ mostrar en un solo alert toda la informacion de la operacion
+ */
