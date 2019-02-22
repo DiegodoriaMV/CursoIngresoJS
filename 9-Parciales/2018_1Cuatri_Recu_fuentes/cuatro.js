cc0a19a333c8a4d92e18825befddb8304670de2f
alumno <alumno>
2019-02-22 09:54:59 -0300

    termine el ejercicio de recufuentes

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,5 +1,124 @@
 function mostrar()
 {
+
+	var importe1;
+	var importe2 ;
+	var importe3 ;
+	var importe4 ;
+	var mayor;
+	var suma;
+	var elTotal;
+
+	importe1 = prompt("ingrese el primer importe");
+	importe2 = prompt("ingrese el segundo importe");
+	importe3 = prompt("ingrese el tercer importe");
+	importe4 = prompt("ingrese el cuarto importe");
+
+	importe1 =parseInt(importe1);
+	importe2 =parseInt(importe2);
+	importe3 =parseInt(importe3);
+	importe4 =parseInt(importe4);
+
+//quien es mayor
+
+	if (importe1 > importe2) 
+	{
+		mayor = importe1
+	
+	} 	else 
+			{	if (importe2 > importe3) 
+				{
+					mayor = importe2
+				}	else
+						{	
+							if (importe3 > importe4) 
+							{
+								mayor = importe3
+							} 	else 	
+									{	if (importe4 > importe1) 
+										{
+											mayor = importe4
+										}
+
+									}
+						}
+
+			}
+
+//        quien es mayor
+
+
+	suma = importe2 + importe4 + importe3+ importe4;
+
+
+	if (suma > 100) 
+		{
+			elTotal = suma * 0.90
+		} else 
+			{
+				if (suma < 100 && suma > 50) 
+					{
+						elTotal = suma * 0.95
+					} else 
+						{
+							if (suma < 50) 
+								{
+									elTotal = suma * 1.15	
+								}
+						}
+			}
+
+
+	alert("la suma da " +suma+ " y el mayor es " + mayor + " y el total a pagar es de " + elTotal ); 
+
+
+
+
+
+
+
+/*
+4 importes
+mostrar el mayor 10 ,20 ,30 ;40  = 40
+
+se suma todo
+supera los 100 se le hace un descuento de 10%
+supera los 50 se le ahce un descuento de 5%
+el maximo al y el precio a pagar
+
+*/
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 	var cantidadDeLibros;
 	var precioDeLoslibros
 	var cuenta;
@@ -23,11 +142,7 @@ function mostrar()
 		precioDeLoslibros = precioDeLoslibros + 0.10;
 	}
 
-	alert(precioDeLoslibros)
-
-
-
-}
+	alert(precioDeLoslibros)*/
 
 /*
  en una tienda de libros existe una oferta por el cual si compra mas de los libros tiene un 
