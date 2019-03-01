79d7af2e28c6580bacfe8af813a9c337a3265011
diegodoriamv <diegodoriamv@hotmail.com>
2019-03-01 12:29:37 -0300

    recu 8 en proceso

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -2,10 +2,69 @@ function mostrar()
 {
 	var pais;
 	var superficie;
+	var paisesImpar;
+	var superficieMCien;
+	var superficieIgualCien;
+	var contador;
+	var paisMayorCien;
+	var sumaDeSuperficie;
+	var promedio;
+	var cantidadDePaisesIng;
 
-	pais = prompt("ingrese el pais");
-	superficie = prompt("ingrese ")
+	cantidadDePaisesIng = 0;
+	superficieMCien = 0;
+	superficieIgualCien = 0;
+	contador = 0;
+	var respuesta = "y";
+	paisesImpar = 0;
 
+
+	while(respuesta != "n")
+	{
+		pais = prompt("ingrese el pais");
+		superficie = prompt("ingrese la superficie")
+		superficie = parseInt(superficie);
+
+
+		if (superficie > 100) 
+		{
+			superficieMCien++;
+		}
+		if(superficie == 100)
+			{
+				superficieIgualCien++;
+			}	
+
+
+
+		if (superficie%2 != 0) 
+		{
+			paisesImpar++;
+		}
+
+		if (superficie > 100) 
+		{
+			if (contador == 0) 
+			{
+				paisMayorCien = pais;
+			}
+			contador++;
+		}
+	
+	cantidadDePaisesIng++;
+	sumaDeSuperficie = sumaDeSuperficie + superficie;
+	respuesta = prompt("para salir aprete la tecla 'n' ");
+	}
+
+	promedio = sumaDeSuperficie / cantidadDePaisesIng;
+	
+	document.write("el pais es " +pais);
+	document.write("<br> su superficie es " +superficie);
+	document.write("<br> los paises impar " + paisesImpar);
+	document.write("<br> la cantidad de paises con superficie igual a 100 son " + superficieIgualCien);
+	document.write("<br> la cantidad de paises con superficie menor a 100 son " + superficieMCien);
+	document.write("<br> el primer pais que supero los 100 es "+ paisMayorCien);
+	document.write("<br> el promedio de la superficie es  " + promedio);
 }
 /*2-en ocho.js, .
 Realizar el algoritmo que permita iterar el ingreso de dos datos, un país y la
